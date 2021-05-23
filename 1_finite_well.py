77372bbea72ac5b717414aa6848ef1948e17e18b
Krzysztof Nowak <krzysztof9nowak@gmail.com>
2021-05-23 22:13:56 +0100

    powoli składamy animacje

--- 1_finite_well.py
+++ 1_finite_well.py
@@ -11,7 +11,7 @@ dx = x[1] - x[0]
 
 d2_dx2 = FinDiff(0, dx, 2, acc=2)
 
-V = np.full(x.shape, 1000.0)
+V = np.full(x.shape, 1e9)
 V[50 : 950] = 0.0
 
 _V = V[:]
@@ -22,21 +22,22 @@ spectrum, states = scipy.sparse.linalg.eigs(H, which='SR', k=5)
 mag = lambda x: (x * np.conj(x)).real
 normalize = lambda x: x / np.linalg.norm(x)
 
-plt.subplot(211)
-plt.plot(x, _V, color="black", linewidth=2)
-plt.ylim(-0.1 , 1)
-plt.ylabel('U(x)')
 
-plt.grid()
+fig, axes = plt.subplots(5)
 
+for n, (energy, state, ax1) in enumerate(list(zip(spectrum, np.transpose(states), axes))):
+    ax2 = ax1.twinx()
 
-plt.subplot(212)
-for n, (energy, state) in enumerate(list(zip(spectrum, np.transpose(states)))[:3]):
-    plt.plot(x, normalize(mag(state)), '-', label=f'n={n} E={energy.real:.1f}')
+    ax1.set_title(f'n={n+1} E={np.real(energy):.2f}')
+    ax1.plot(x, normalize(mag(state)), '-', color='crimson')
+    ax1.set_ylabel('$\Psi^2(x)$', color='crimson')
+    ax1.get_xaxis().set_visible(False)
+    ax1.tick_params(left=False, labelleft=False)
 
-plt.ylabel('$\Psi^2(x)$')
-plt.xlabel('x')
-plt.grid()
-plt.legend(loc='best')
+    
+    ax2.plot(x, _V, color="black", linewidth=2, label='U(x)')
+    ax2.set_ylabel('U(x)')
+    ax2.tick_params(right=False, labelright=False)
+
 
 plt.show()
